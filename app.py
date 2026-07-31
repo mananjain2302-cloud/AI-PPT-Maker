77a8af0301a2f2c38b556247bcf8433f5fd8c02a
mananjain2302-cloud <mananjain2302@gmail.com>
2026-07-31 13:19:02 +0530

    Update app.py

--- app.py
+++ app.py
@@ -141,7 +141,7 @@ if (user_input) and (leader_agent):
                                                        'content':prompt}]})
             code = response['messages'] [-1].content[-1]['text']
             st.html(code, width="stretch",
-                    unsafe_allow_javascript=True)S
+                    unsafe_allow_javascript=True)
             except Exception as err:
             st.error(err)
   # tab 3 code:
