b8f851724019c0da9e4b7884954d9171d2fcc6fa
mananjain2302-cloud <mananjain2302@gmail.com>
2026-07-31 13:23:06 +0530

    Update app.py

--- app.py
+++ app.py
@@ -153,7 +153,7 @@ if (user_input) and (leader_agent):
           st.html(code, width="stretch",
                   unsafe_allow_javascript=True)
         # file save
-        with open("ppt.html","w") as f:
+        with open("ppt.html","w"):
           f.write(code)
           
           st.download_button(label="DOWNLOAD PPT",
